40e68de3c72561d2bfa64c388b93fc77b79d994a
AshNicolus <yashnema52@gmail.com>
2025-03-19 09:37:15 +0530

    Trees

---
+++ Trees/LCA.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.x = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or root == p or root == q:
+            return root
+        # if root.left == p and root.right == q:
+        #     return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if left and right:
+            return root
+        return left if left else right
